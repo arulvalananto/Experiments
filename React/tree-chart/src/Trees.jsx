d662882a6ff67f76672f6f9dc72f275ff8d6a075
Arul Valan Anto S <61511921+arulvalananto@users.noreply.github.com>
2021-12-01 03:27:00 +0530

    Web crawler deleted

--- React/tree-chart/src/Trees.jsx
+++ React/tree-chart/src/Trees.jsx
@@ -1,24 +1,21 @@
 import { Tree, TreeNode } from "react-organizational-chart";
 import { MdClear } from "react-icons/md";
 
-import data from "./data";
+// import data from "./data";
 import "./Trees.css";
 
+import data from "./chart-test1.json";
+import ZoomBox from "./ZoomBox";
+
 const PrintChild = ({ data }) => {
-  if (!data)
-    return (
-      <TreeNode
-        label={
-          <div className="endNode">
-            <MdClear />
-          </div>
-        }
-      />
-    );
+  if (!data) return <h2>End</h2>;
 
   return data?.map((child, index) => (
-    <TreeNode key={index} label={<div className="childNode">{child.name}</div>}>
-      <PrintChild data={child.children} />
+    <TreeNode
+      key={index}
+      label={<div className="childNode">{child?.name}</div>}
+    >
+      <PrintChild data={child?.childs} />
     </TreeNode>
   ));
 };
@@ -29,9 +26,9 @@ const Trees = () => {
       lineWidth={"2px"}
       lineColor={"red"}
       lineBorderRadius={"10px"}
-      label={<div className="userNode">{data.name}</div>}
+      label={<div className="userNode">{data.output.name}</div>}
     >
-      <PrintChild data={data.children} />
+      <PrintChild data={data.output.childs} />
     </Tree>
   );
 };
